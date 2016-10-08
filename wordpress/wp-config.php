d0d428171ed8b8ee85b1b9af47009c319bfe260d
Erik Wright <kirashi3@users.noreply.github.com>
2016-10-08 01:13:40 -0700

    added wp-config example file
    contains useful wp-config options and variables

---
+++ wordpress/wp-config.php
@@ -0,0 +1,7 @@
+/* Disables Backend File Editor */
+define('DISALLOW_FILE_EDIT', TRUE);
+
+/* Frontend Memory Limit */
+define( 'WP_MEMORY_LIMIT', '96M' );
+/* Backend Memory Limit */
+define( 'WP_MAX_MEMORY_LIMIT', '128M' );
