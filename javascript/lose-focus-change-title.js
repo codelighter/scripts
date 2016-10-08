a35eb4b627e5c90c3812f42a87e5eb8839bc833a
Erik Wright <kirashi3@users.noreply.github.com>
2016-10-08 01:39:52 -0700

    added change title on lose focus script
    changes website title upon losing focus
    kind of neat

---
+++ javascript/lose-focus-change-title.js
@@ -0,0 +1,18 @@
+<script>
+//changes website title upon losing focus
+$(function () {
+  var message = "Hey, come back!";
+  var original;
+
+  $(window).focus(function() {
+  if(original) {
+  document.title = original;
+  }
+  }).blur(function() {
+  var title = $('title').text();
+  if(title != message) {
+  original = title;
+  }
+  document.title = message;
+  });
+});</script>
