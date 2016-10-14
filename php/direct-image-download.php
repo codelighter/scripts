d06ec1249a20d6f7ef2c41f0bb37973b0f929dbb
Erik Wright <erik_is_cool77@hotmail.com>
2016-10-13 22:22:14 -0700

    Fixed errors & prevented direct script access
    - changed handling of variable in_array check (LINE:48)
    -- (in_array(end(explode('.',$fileName)));) now handled on multiple
    lines
    - added !defined('ABSPATH') exit to prevent direct access (LINE:92)

--- php/direct-image-download.php
+++ php/direct-image-download.php
@@ -3,7 +3,6 @@
 
 
 
-
 //================================ START ====================================//
 /* modified single-project.php allows direct download of featured post image
  * this version created 2016-10-08 by EWTech for the Divi Theme Users FB Group
@@ -46,38 +45,40 @@ $file_extension = pathinfo($file_name);
 //security check
 $fileName = strtolower($file_url);
 $whitelist = array('png', 'gif', 'tiff', 'jpeg', 'jpg','bmp','svg');
-if(!in_array(end(explode('.', $fileName)), $whitelist)) {
-    exit('Invalid file!');
+$tmp = explode('.', $fileName);
+$fileName = end($tmp);
+if(!in_array($fileName, $whitelist)) {
+  exit('Invalid file!');
 }
 if(strpos( $file_url , '.php' ) == true) {
-    die("Invalid file!");
+  die("Invalid file!");
 }
 
 //check if file exist
 if(file_exists( $file_url  ) == false) {
-    #exit("File Not Found!");
+  #exit("File Not Found!");
 }
 
 //rename (since 1.4)
 if(isset($file_new_name) and !empty($file_new_name)) {
-    $file_new_name = $file_new_name.".".$file_extension['extension'];
+  $file_new_name = $file_new_name.".".$file_extension['extension'];
 } else{
-    $file_new_name = $file_name;
+  $file_new_name = $file_name;
 }
 
 //check filetype
 switch( $file_extension['extension'] ) {
-        case "png": $content_type="image/png"; break;
-        case "gif": $content_type="image/gif"; break;
-        case "tiff": $content_type="image/tiff"; break;
-        case "jpeg":
-        case "jpg": $content_type="image/jpg"; break;
-        default: $content_type="application/force-download";
+    case "png": $content_type="image/png"; break;
+    case "gif": $content_type="image/gif"; break;
+    case "tiff": $content_type="image/tiff"; break;
+    case "jpeg":
+    case "jpg": $content_type="image/jpg"; break;
+    default: $content_type="application/force-download";
 }
 header("Expires: 0");
-header("Cache-Control: no-cache, no-store, must-revalidate");
-header('Cache-Control: pre-check=0, post-check=0, max-age=0', false);
-header("Pragma: no-cache");   
+header("Cache-Control: no-cache, no-store, must-revalidate"); 
+header('Cache-Control: pre-check=0, post-check=0, max-age=0', false); 
+header("Pragma: no-cache");  
 header("Content-type: {$content_type}");
 header("Content-Disposition:attachment; filename={$file_new_name}");
 header("Content-Type: application/force-download");
@@ -88,6 +89,7 @@ exit();
 
 // resumes normal page operations if post data not found
 else:
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 //================================ END ====================================//
 
 
@@ -106,22 +108,22 @@ $show_navigation = get_post_meta( get_the_ID(), '_et_pb_project_nav', true );
 
 <?php if ( ! $is_page_builder_used ) : ?>
 
-    <div class="container">
-        <div id="content-area" class="clearfix">
-            <div id="left-area">
+  <div class="container">
+    <div id="content-area" class="clearfix">
+      <div id="left-area">
 
 <?php endif; ?>
 
-            <?php while ( have_posts() ) : the_post(); ?>
+      <?php while ( have_posts() ) : the_post(); ?>
 
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-                <?php if ( ! $is_page_builder_used ) : ?>
+        <?php if ( ! $is_page_builder_used ) : ?>
 
-                    <div class="et_main_title">
-                        <h1 class="entry-title"><?php the_title(); ?></h1>
-                        <span class="et_project_categories"><?php echo get_the_term_list( get_the_ID(), 'project_category', '', ', ' ); ?></span>
-                    </div>
+          <div class="et_main_title">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <span class="et_project_categories"><?php echo get_the_term_list( get_the_ID(), 'project_category', '', ', ' ); ?></span>
+          </div>
 
 
 
@@ -139,21 +141,21 @@ $show_navigation = get_post_meta( get_the_ID(), '_et_pb_project_nav', true );
 
 
 
-                <?php
-                    $thumb = '';
+        <?php
+          $thumb = '';
 
-                    $width = (int) apply_filters( 'et_pb_portfolio_single_image_width', 1080 );
-                    $height = (int) apply_filters( 'et_pb_portfolio_single_image_height', 9999 );
-                    $classtext = 'et_featured_image';
-                    $titletext = get_the_title();
-                    $thumbnail = get_thumbnail( $width, $height, $classtext, $titletext, $titletext, false, 'Projectimage' );
-                    $thumb = $thumbnail["thumb"];
+          $width = (int) apply_filters( 'et_pb_portfolio_single_image_width', 1080 );
+          $height = (int) apply_filters( 'et_pb_portfolio_single_image_height', 9999 );
+          $classtext = 'et_featured_image';
+          $titletext = get_the_title();
+          $thumbnail = get_thumbnail( $width, $height, $classtext, $titletext, $titletext, false, 'Projectimage' );
+          $thumb = $thumbnail["thumb"];
 
-                    $page_layout = get_post_meta( get_the_ID(), '_et_pb_page_layout', true );
+          $page_layout = get_post_meta( get_the_ID(), '_et_pb_page_layout', true );
 
-                    if ( '' !== $thumb )
-                        print_thumbnail( $thumb, $thumbnail["use_timthumb"], $titletext, $width, $height );
-                ?>
+          if ( '' !== $thumb )
+            print_thumbnail( $thumb, $thumbnail["use_timthumb"], $titletext, $width, $height );
+        ?>
 
 
 
@@ -161,7 +163,7 @@ $show_navigation = get_post_meta( get_the_ID(), '_et_pb_project_nav', true );
 
 <?php //================================ START ====================================// ?>
 <form id="imgdl" name="imgdl" method="post" action="<?php echo htmlspecialchars(get_stylesheet_directory_uri().'/single-project.php'); ?>">
-  <input name="new_name" type="hidden" value="<?php //whatever you put here renames the files upon download ?>" />
+  <input name="new_name" type="hidden" value="" /><?php //give this input a value to rename the file on download ?>
   <input name="pic_url" type="hidden" value="<?php echo the_post_thumbnail_url( 'full' ); ?>" />
   <span class="et_overlay" onclick="imgdl.submit()"></span>
 </form>
@@ -172,49 +174,49 @@ $show_navigation = get_post_meta( get_the_ID(), '_et_pb_project_nav', true );
 
 
 
-                <?php endif; ?>
+        <?php endif; ?>
 
-                    <div class="entry-content">
-                    <?php
-                        the_content();
+          <div class="entry-content">
+          <?php
+            the_content();
 
-                        if ( ! $is_page_builder_used )
-                            wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
-                    ?>
-                    </div> <!-- .entry-content -->
+            if ( ! $is_page_builder_used )
+              wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
+          ?>
+          </div> <!-- .entry-content -->
 
-                <?php if ( ! $is_page_builder_used ) : ?>
+        <?php if ( ! $is_page_builder_used ) : ?>
 
-                    <?php if ( 'et_full_width_page' !== $page_layout ) et_pb_portfolio_meta_box(); ?>
+          <?php if ( 'et_full_width_page' !== $page_layout ) et_pb_portfolio_meta_box(); ?>
 
-                <?php endif; ?>
+        <?php endif; ?>
 
-                <?php if ( ! $is_page_builder_used || ( $is_page_builder_used && 'on' === $show_navigation ) ) : ?>
+        <?php if ( ! $is_page_builder_used || ( $is_page_builder_used && 'on' === $show_navigation ) ) : ?>
 
-                    <div class="nav-single clearfix">
-                        <span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . et_get_safe_localization( _x( '&larr;', 'Previous post link', 'Divi' ) ) . '</span> %title' ); ?></span>
-                        <span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . et_get_safe_localization( _x( '&rarr;', 'Next post link', 'Divi' ) ) . '</span>' ); ?></span>
-                    </div><!-- .nav-single -->
+          <div class="nav-single clearfix">
+            <span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . et_get_safe_localization( _x( '&larr;', 'Previous post link', 'Divi' ) ) . '</span> %title' ); ?></span>
+            <span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . et_get_safe_localization( _x( '&rarr;', 'Next post link', 'Divi' ) ) . '</span>' ); ?></span>
+          </div><!-- .nav-single -->
 
-                <?php endif; ?>
+        <?php endif; ?>
 
-                </article> <!-- .et_pb_post -->
+        </article> <!-- .et_pb_post -->
 
-            <?php
-                if ( ! $is_page_builder_used && comments_open() && 'on' == et_get_option( 'divi_show_postcomments', 'on' ) )
-                    comments_template( '', true );
-            ?>
-            <?php endwhile; ?>
+      <?php
+        if ( ! $is_page_builder_used && comments_open() && 'on' == et_get_option( 'divi_show_postcomments', 'on' ) )
+          comments_template( '', true );
+      ?>
+      <?php endwhile; ?>
 
 <?php if ( ! $is_page_builder_used ) : ?>
 
-            </div> <!-- #left-area -->
+      </div> <!-- #left-area -->
 
-            <?php if ( 'et_full_width_page' === $page_layout ) et_pb_portfolio_meta_box(); ?>
+      <?php if ( 'et_full_width_page' === $page_layout ) et_pb_portfolio_meta_box(); ?>
 
-            <?php get_sidebar(); ?>
-        </div> <!-- #content-area -->
-    </div> <!-- .container -->
+      <?php get_sidebar(); ?>
+    </div> <!-- #content-area -->
+  </div> <!-- .container -->
 
 <?php endif; ?>
 
